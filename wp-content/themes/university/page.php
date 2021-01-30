ceb2ca4bb9049adeec7868037797e2158a3e54e8
sloba82 <dizajnistampa@gmail.com>
2021-01-30 13:25:29 +0100

    page

--- wp-content/themes/university/page.php
+++ wp-content/themes/university/page.php
@@ -17,9 +17,21 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
+<? var_dump(wp_get_post_parent_id(get_the_ID()));?>
+
+
+    <?php  if (wp_get_post_parent_id(get_the_ID())) { ?>
+
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <a href=""><span class="metabox__main"><?= the_title();?></span></a></p>
+      </div>
+
+    <?php
+       }
+    ?>
+
+
+
     
     <!-- <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -42,4 +54,4 @@
 
   get_footer();
 
-?>
+?> 
